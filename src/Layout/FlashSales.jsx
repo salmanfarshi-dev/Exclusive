33e968cd8917a07576053ae9491da396b5244ae7
Salman Farshi <sfarshi222@gmail.com>
2026-05-27 23:14:01 +0600

    fix vercel routing

--- src/Layout/FlashSales.jsx
+++ src/Layout/FlashSales.jsx
@@ -1,30 +1,58 @@
-import Subheading from '../Component/Subheading'
-import Today  from '../Component/Today'
-import React from 'react'
-import Cards from '../Component/Cards'
-import Keyboard from '../assets/keyboard.png'
-import Button from '../Component/Button'
-import Container  from '../Component/Container'
+import Subheading from "../Component/Subheading";
+import Today from "../Component/Today";
+import React from "react";
+import Cards from "../Component/Cards";
+import Keyboard from "../assets/keyboard.png";
+import Button from "../Component/Button";
+import Container from "../Component/Container";
 
 function FlashSales() {
   return (
-   <section className="mb-20">
-   <Container >
-     <Today text="Today’s"/>
-    <Subheading  text="Flash Sales" className="mt-6"/>
+    <section className="mb-20">
+      <Container>
+        <Today text="Today’s" />
+        <Subheading text="Flash Sales" className="mt-6" />
 
-    <div className="mt-10 flex justify-between items-center">
-      <Cards src={Keyboard} alt="Image" tittle="HAVIT HV-G92 Gamepad" badge="-35%" discount="120" regular="160"/>
-      <Cards src={Keyboard} alt="Image" tittle="HAVIT HV-G92 Gamepad" badge="-35%" discount="120" regular="160"/>
-      <Cards src={Keyboard} alt="Image" tittle="HAVIT HV-G92 Gamepad" badge="-35%" discount="120" regular="160"/>
-      <Cards src={Keyboard} alt="Image" tittle="HAVIT HV-G92 Gamepad" badge="-35%" discount="120" regular="160"/>
-    </div>
-    <div className="text-center mt-16 pb-16 border-b border-[#8282824d]">
-      <Button text="View All Products"/>
-    </div>
-   </Container>
-   </section>
-  )
+        <div className="mt-10 flex justify-between items-center">
+          <Cards
+            src={Keyboard}
+            alt="Image"
+            tittle="HAVIT HV-G92 Gamepad"
+            badge="-35%"
+            discount="120"
+            regular="160"
+          />
+          <Cards
+            src={Keyboard}
+            alt="Image"
+            tittle="HAVIT HV-G92 Gamepad"
+            badge="-35%"
+            discount="120"
+            regular="160"
+          />
+          <Cards
+            src={Keyboard}
+            alt="Image"
+            tittle="HAVIT HV-G92 Gamepad"
+            badge="-35%"
+            discount="120"
+            regular="160"
+          />
+          <Cards
+            src={Keyboard}
+            alt="Image"
+            tittle="HAVIT HV-G92 Gamepad"
+            badge="-35%"
+            discount="120"
+            regular="160"
+          />
+        </div>
+        <div className="text-center mt-16 pb-16 border-b border-[#8282824d]">
+          <Button text="View All Products" />
+        </div>
+      </Container>
+    </section>
+  );
 }
 
-export default FlashSales
+export default FlashSales;
